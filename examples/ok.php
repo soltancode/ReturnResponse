678eec45208bf47e1602e6e254709e27b62aba2e
Soltan Abilgasimzada <soltan.ebilqasimzade@gmail.com>
2022-09-28 17:32:18 +0400

    Update ok example.

--- examples/ok.php
+++ examples/ok.php
@@ -2,8 +2,15 @@
 
 require '../vendor/autoload.php';
 
+# If you use facade, import this.
+use Soltancode\ReturnResponse\Facades\ReturnResponse;
+
 $httpResponse = 200;
 $data         = ['success' => true];
 $message      = "Operation done successfully.";
 
+# Using as facade.
+return ReturnResponse::response($httpResponse, $data, $message);
+
+# Using as helper.
 return returnResponse()->response($httpResponse, $data, $message);
